aebd957551cf32590456e43dc09c86333953d89d
Ning Sun <sunng@protonmail.com>
2019-02-01 00:13:27 +0800

    feat(purgatory) added one-one multiplex purgatory

---
+++ src/link_async/multiplex.clj
@@ -0,0 +1,29 @@
+(ns link-async.multiplex
+  (:require [link-async.core :as la]))
+
+(deftype MultiplexPurgatory [txn-id-fn state]
+
+  la/IPurgatory
+  (reset-state! [this]
+    (reset! state {}))
+
+  (get-chan [this packet]
+    (when-let [txn-id (txn-id-fn packet)]
+      (@state txn-id)))
+
+  (start-transaction! [this packet chan]
+    (swap! state assoc (txn-id-fn packet) chan))
+
+  (end-transaction! [this packet]
+    (swap! state dissoc (txn-id-fn packet)))
+
+  (beginning? [this packet]
+    (if-let [txn-id (txn-id-fn packet)]
+      (contains? @state txn-id)
+      false))
+
+  (terminate? [this packet]
+    (some? (txn-id-fn packet))))
+
+(defn multiplex-purgatory [txn-id-fn]
+  (MultiplexPurgatory. txn-id-fn (atom {})))
