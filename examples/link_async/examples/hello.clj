49ccbd9611f0137f63aa6934db2b54c106c44e8e
Ning Sun <sunng@protonmail.com>
2019-01-31 20:48:30 +0800

    test(example) add server to example

--- examples/link_async/examples/hello.clj
+++ examples/link_async/examples/hello.clj
@@ -13,12 +13,23 @@
    (codec/frame
     (codec/string :encoding "utf8" :delimiter "\r\n"))))
 
-(defn -main [& args]
-  (let [handler (linka/handler-factory link-pipeline/pipeline-purgatory)
-        client-factory (tcp/tcp-client-factory [line-based-codec handler])
-        the-client (tcp/tcp-client client-factory "127.0.0.1" 8715 :lazy-connect false)
+(def server-handler
+  (link/create-handler
+   (link/on-message [ch msg]
+                    (link/send! ch msg))))
 
+(defn -main [& args]
+  ;; start server
+  (tcp/tcp-server 8715 [line-based-codec server-handler])
+  (let [;; client handler
+        handler (linka/handler-factory link-pipeline/pipeline-purgatory)
+        ;; client bootstrap
+        client-factory (tcp/tcp-client-factory [line-based-codec handler])
+        ;; actual client
+        the-client (tcp/tcp-client client-factory "127.0.0.1" 8715 :lazy-connect false)
+        ;; send the request and get the chan
         the-chan (linka/send-async! the-client ["Link async example!\r\n"])]
+    ;; get response from the chan
     (async/go
       (println (async/<! the-chan)))
     (Thread/sleep 5000)))
