a518a3a76ac2ed65b043256f0cd0bd69cd82ea7c
Ning Sun <sunng@protonmail.com>
2019-10-22 23:04:36 +0800

    (feat) update link

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [link "0.12.2"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [link "0.12.4"]
                  [org.clojure/core.async "0.4.490"]
                  [org.clojure/tools.logging "0.4.1"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}
