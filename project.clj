155f20c614622ff68c1e49da4700be4a3271b6fe
Ning Sun <sunng@protonmail.com>
2020-04-30 22:55:12 +0800

    (feat) deps up

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [link "0.12.4"]
-                 [org.clojure/core.async "0.4.490"]
-                 [org.clojure/tools.logging "0.4.1"]]
+                 [link "0.12.5"]
+                 [org.clojure/core.async "1.1.587"]
+                 [org.clojure/tools.logging "1.1.0"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}
              :example {:source-paths ["examples"]}}
   :repl-options {:init-ns link-async.core}
