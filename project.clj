c0160819f3a94a4d876a5b0e34befe914ee42d38
Ning Sun <sunng@protonmail.com>
2021-08-29 21:55:11 +0800

    feat: Bump dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [link "0.12.5"]
-                 [org.clojure/core.async "1.1.587"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [link "0.12.7"]
+                 [org.clojure/core.async "1.3.618"]
                  [org.clojure/tools.logging "1.1.0"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}
              :example {:source-paths ["examples"]}}
